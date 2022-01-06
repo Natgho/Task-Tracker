2c2960415389ae5846472c046d61dca21c9790e2
Sezer Bozkır <admin@sezerbozkir.com>
2022-01-06 11:36:19 +0300

    mock server is added. Task add/delete/update props are added

--- src/App.js
+++ src/App.js
@@ -5,27 +5,8 @@ import Tasks from "./Components/Tasks";
 import AddTask from "./Components/addTask";
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: "Doctor Appointment",
-            day: "Feb 5th at 2:30pm",
-            reminder: true
-        },
-        {
-            id: 2,
-            text: "Meeting at School",
-            day: "Feb 6th at 2:30pm",
-            reminder: true
-        },
-        {
-            id: 3,
-            text: "Food Shopping",
-            day: "Feb 7th at 2:30pm",
-            reminder: true
-        }
-    ])
-
+    const [tasks, setTasks] = useState([])
+    const [showAdd, setShowAdd] = useState(false)
     // Delete Task
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id))
@@ -36,10 +17,16 @@ function App() {
         setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
     }
 
+    //Add Task
+    const addTask = (task) => {
+        const newTask = {id: tasks.length + 1, ...task}
+        setTasks([...tasks, newTask]);
+    }
+
     return (
         <div className="container">
-            <Header title="Task Tracker"/>
-            <AddTask/>
+            <Header title="Task Tracker" showAdd={showAdd} setShowAdd={setShowAdd}/>
+            {showAdd && <AddTask onAdd={addTask}/>}
             {tasks.length > 0 ? <Tasks tasks={tasks}
                                        onDelete={deleteTask}
                                        onToggle={toggleReminder}/> : 'No Tasks to Show'}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from "./Button";
-import Tasks from "./Tasks";
 
-const Header = ({title}) => {
+const Header = ({title, showAdd, setShowAdd}) => {
     const onClick = () => {
-        console.log("click")
+        setShowAdd(!showAdd)
     }
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button color={showAdd ? "red": "green"} text={showAdd ? "Close": "Add"} onClick={onClick}/>
         </header>
 
     );
